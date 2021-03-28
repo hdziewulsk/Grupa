e902965704962a5988ebb8aed2287a8079d40acd
izczum <izabelaczumak@student.kul.lublin.pl>
2021-03-28 11:03:38 +0200

    moj pierszy commit - cherry pick

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Hello World!");
+        System.out.println("Iza linia");
         System.out.println("Agnieszka drzewko");
 
     }
